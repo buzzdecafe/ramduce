9a706aa4d5d8f74bc05b1ca629f85bce6c8681f3
buzzdecafe <m_hur@yahoo.com>
2014-12-08 10:00:04 -0500

    taking a step back and thinking about it

--- src/notes.js
+++ src/notes.js
@@ -1,47 +1,158 @@
 // Notes
 
+// Hickey talk notes
+//
+// using my idiosyncratic type notation ...
 /*
-So...
-we all know that `map` and `filter` (and other list functions) can be implemented in terms of reduce. 
-So here is reduce. It takes an arity-2 reducing function, an accumulator, and a list:
-*/
+put the baggage on the plane 
+  as you do that:
+    break apart pallets (convert pallets of bags into sets of individual bags: 
+        chain/flatMap/mapcat)
+    remove bags that smell like food (filter)
+    label heavy bags (map)
 
-function reduce(fn, acc, ls) {
-  return (ls.length === 0) ? acc : 
-    reduce(fn, fn(acc, head(ls)), tail(ls));
+pipe(
+  chain(unbundlePallet),
+  filter(nonFood),
+  map(labelHeavy)
+);
+  
+How can we use transducers to eliminate intermediate structures?
+
+// Transducer T processBags :: Pallets(?) -> T Bags 
+// is that sig right?
+processBags = pipe(
+  chaining(unbundlePallet), // T chaining :: T(?) -> T
+  filtering(nonFood),       // T filtering :: T(?) -> T
+  mapping(labelHeavy)       // T mapping :: T(?) -> T
+);
+
+Transducer T is a function that takes a transforming/reducing function (acc, x -> acc) 
+and returns another Transducer that wraps and transforms that reducing function:
+
+Transducer T :: (acc, x -> acc) -> T (acc, x -> acc)'
+
+into(airplane, processBags(pallets));
+
+`transduce` is like reduce but takes transducers:
+transduce :: T -> T
+
+@example:
+
+  transduce(compose(processBags, mapping(weighBag)), add, 0,            pallets);
+            ^       ^            ^                   ^    ^             ^
+            |       |            |                   |    |             |
+          T -> T  T -> T       T -> T       reducing fn  accumulator   data
+
+The transducer can transform the internal reducing function. That is the point.
+
+Defining list functions as fold (right):
+
+function mapr(fn, ls) {
+  return foldr(function(acc, x) { return prepend(f(x), acc); }, [], ls);
 }
 
-/*
-`map` is simply a reduce where the accumulator is another list, and the reducing function is `concat`. 
-what gives `map` its special flavor is the transforming function `a -> b` that it takes as its first 
-argument (`f` in the example below): 
-*/
-
-function map(f, ls) {
-  return (ls.length === 0) ? [] : 
-    reduce(concat, [f(head(ls))], tail(ls));
+function filterr(pred, ls) {
+  return foldr(function(acc, x) { return pred(x) ? prepend(x, acc) : acc; }, [], ls);
 }
 
-/*
-similarly for filter, the reducing function is `concat`, and the accumulator is another list. What 
-distinguishes `filter` is that it takes a predicate which decides which elements of the input list 
-make it to the output list. Here is `filter` in terms of `reduce`:
-*/
+What is different here? 
+  mapr -> return prepend(f(x), acc);
+  filterr -> return pred(x) ? prepend(x, acc) : acc 
 
-function filter(pred, ls) {
-  return (ls.length === 0) ? [] :
-    reduce(concat, (pred(head(ls)) ? [head(ls)] : []), tail(ls));
+What is the same?
+  EVERYTHING ELSE
+
+Let's implement as left fold:
+
+function mapl(fn, ls) {
+  return foldl(function(acc, x) { return append(f(x), acc); }, [], ls);
 }
 
-/*
-Both `map` and `filter` take a function and a list. Both rely on `concat` to assemble their output 
-list. What is different is how the accumulate their output. Let's see if we can get a lift by 
-factoring out the reducing function (i.e. `concat`), and passing in a function to populate the
-accumulator:
-*/
-
-function ???(stepFn, initFn) {
-  return function(fn, ls) {
-    return ls.length === 0 ? [] :
-      reduce(stepFn, initFn(head(ls)), tail(ls));
+function filterl(pred, ls) {
+  return foldl(function(acc, x) { return pred(x) ? append(x, acc) : acc; }, [], ls);
 }
+
+function chainl(fn, ls) {
+  return foldl(function(acc, x) { 
+    return reduce(append, acc, fn(x));
+  }, [], ls);
+}
+
+right fold -> Laziness path
+left fold -> Looping, eager path
+
+All of the implementations rely on `append` -- iteration logic has leaked into the reduction.
+Let's get it out of there. We can pass `append` -- or any reducing-type function, i.e. with 
+the signature (acc, x -> acc) -- in an decouple it from the reduction.
+
+parameterize the "step function" (`append` in the examples above:
+
+function mapping(fn) {        // pass in the transforming function
+  return function(step) {     // <--- pass reducing function (e.g. `append` here)
+    return function(acc, x) { // and get out a new reducing function (acc, x -> acc)
+      return step(acc, fn(x));
+    };
+  };
+}
+
+this could be rewritten for ramda using currying:
+
+// mapping :: (a -> b) -> ([b], a -> [b]) -> ([b], a -> [b]) 
+mapping = curry(function(fn, step) {
+  return function(acc, x) {
+    return step(acc, fn(x));
+  };
+});
+
+Likewise for filtering and chaining:
+
+// filtering :: (a -> Boolean) -> ([b], a -> [b]) -> ([b], a -> [b]) 
+filtering = curry(function(pred, step) {
+  return function(acc, x) {
+    return pred(x) ? step(acc, x) : acc;
+  };
+});
+
+// chaining :: (a -> [b]) -> ([b], a -> [b]) -> ([b], a -> [b]) 
+chaining = curry(function(pred, step) {
+  return function(acc, x) {
+    return foldl(step, acc, x);
+  };
+});
+
+Now we can redfine `mapl` et al. with a transducer that takes a step function (in 
+this case `append`):
+
+function mapl(fn, ls) {
+  return foldl(mapping(fn, append), [] ls);
+}
+
+function filterl(fn, ls) {
+  return foldl(filtering(fn, append), [] ls);
+}
+
+function chainl(fn, ls) {
+  return foldl(chaining(fn, append), [] ls);
+}
+
+Now composition ... flows both ways?!?!
+
+compose(
+    chaining(unbundlePallet),
+    filtering(nonFood),
+    map(labelHeavy)
+);
+
+this flows right-to-left as expected for composition, "but it builds a transformation
+step that runs in the order that they appear, left-to-right in the [composition]."
+
+!!!!
+
+
+
+
+
+
+
+*/
---
+++ study/README.md
@@ -0,0 +1,21 @@
+Resources for study
+===================
+
+* [A tutorial on the universality and expressiveness of fold](http://www.cs.nott.ac.uk/~gmh/fold.pdf)
+  Graham Hutton
+
+* [Lectures on Constructive Functional Programming](http://www.cs.ox.ac.uk/files/3390/PRG69.pdf)
+  R.S. Bird
+
+* [Transducers Are Coming](http://blog.cognitect.com/blog/2014/8/6/transducers-are-coming) Rich Hickey
+
+* [Transducers (video)](https://www.youtube.com/watch?v=6mTbuzafcII) Rich Hickey
+
+* [Transducers (docs)](http://clojure.org/transducers) 
+
+* [Transducers Esxplained: Part 1](http://simplectic.com/blog/2014/transducers-explained-1/) and
+  [Transducers Explained: Pipelines](http://simplectic.com/blog/2014/transducers-explained-pipelines/)
+  Kevin Beatty
+
+
+
