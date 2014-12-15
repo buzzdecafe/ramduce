0118245c9894e388400976ee434e579d6aa66e9d
Kevin Beaty <kevin@simplectic.com>
2014-12-15 17:25:17 -0600

    First stab at supporting transducer within map... untested... does it work?

--- src/rmap.js
+++ src/rmap.js
@@ -1,3 +1,5 @@
+var symbolExists = typeof Symbol !== 'undefined',
+    symTransformer = symbolExists ? Symbol('transformer') : '@@transformer';
 
 var _isArray = Array.isArray || function _isArray(val) {
     return (val != null &&
@@ -8,6 +10,27 @@ var _isArray = Array.isArray || function _isArray(val) {
 
 function _hasMethod(methodName, obj) {
     return obj != null && !_isArray(obj) && typeof obj[methodName] === 'function';
+}
+
+function _isFunction(obj){
+  return typeof obj === 'function';
+}
+
+function _isTransformer(obj){
+    return obj != null &&
+      (value[symTransformer] !== undef) ||
+        (_isFunction(value.step) && _isFunction(value.result));
+}
+
+function _transformer(value){
+  var xf;
+  if(isTransformer(value)){
+    xf = value[symTransformer];
+    if(xf === undef){
+      xf = value;
+    }
+  }
+  return xf;
 }
 
 function _curry2(fn) {
@@ -29,6 +52,8 @@ function _curry2(fn) {
 module.exports = _curry2(function _map(fn, list) {
     if (_hasMethod('map', list)) {
         return list.map(fn);
+    } else if(_isTransformer(list)){
+        return new Map(fn, list);
     }
     var idx = -1, len = list.length, result = new Array(len);
     while (++idx < len) {
@@ -36,3 +61,19 @@ module.exports = _curry2(function _map(fn, list) {
     }
     return result;
 });
+
+function Map(f, xf) {
+  this.xf = xf;
+  this.f = f;
+}
+Map.prototype.init = function(){
+  return this.xf.init();
+};
+Map.prototype.result = function(result){
+  return this.xf.result(result);
+};
+Map.prototype.step = function(result, input) {
+  return this.xf.step(result, this.f(input));
+};
+
+module.exports = map
