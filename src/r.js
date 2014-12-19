d562da5e5537cf73f541c6a9c9b4dac9bf275bc9
buzzdecafe <m_hur@yahoo.com>
2014-12-19 18:08:03 -0500

    incl. iteration

--- src/r.js
+++ src/r.js
@@ -1,5 +1,9 @@
 var R = {};
 
+function _hasMethod(name, obj) {
+     return obj != null && !Array.isArray(obj) && typeof obj[name] === 'function'
+}
+
 function _curry2(fn) {
     return function(a, b) {
       switch (arguments.length) {
@@ -34,6 +38,29 @@ function _curry3(fn) {
     };
 }
 
+function _curry4(fn) {
+    return function(a, b, c, d) {
+      switch (arguments.length) {
+        case 0:
+          throw _noArgsException();
+        case 1:
+          return _curry2(function(b, c, d) {
+            return fn(a, b, c, d);
+          });
+        case 2:
+          return function(c, d) {
+            return fn(a, b, c, d);
+          };
+        case 3:
+          return function(d) {
+            return fn(a, b, c, d);
+          };
+        default:
+          return fn(a, b, c, d);
+      }
+    };
+}
+
 function init() {
     return this.xf.init();
 }
@@ -42,50 +69,106 @@ function result(x) {
      return this.xf.result(x); 
 }
 
+function arrayReduce(xf, acc, ls) {
+    var i = -1, len = ls.length;
+    while(++i < len) {
+        acc = xf.step(acc, ls[i]);
+        if (acc.__transducers_reduced__) {
+            acc = acc.value;
+            break;
+        }
+    }
+    return xf.result(acc);
+}
+
+function iterableReduce(xf, acc, iter) {
+    if(iter["@@iterator"]) {
+        iter = iter["@@iterator"]();
+    }
+    var step = iter.next();
+    while(!step.done) {
+      acc = xf.step(acc, step.value);
+      if(iacc.__transducers_reduced__) {
+        acc = acc.value;
+        break;
+      }
+      step = iter.next();
+    }
+    return xf.result(acc);
+};
+
 //-----------------------------------------------
 // MAPPING
 var Map = _curry2(function Map(f, xf) {
-  this.f = f;
-  this.xf = xf;
+    this.f = f;
+    this.xf = xf;
 });
 Map.prototype.init = init;
 Map.prototype.result = result;
 Map.prototype.step = function(result, input) {
-  return this.xf.step(result, this.f(input));
+    return this.xf.step(result, this.f(input));
 };
-R.map = _curry2(function map(fn, ls) {});
+R.map = _curry2(function map(fn, ls) {
+    // functor dispatch, excluding array
+    if (hasMethod('map', ls)) {
+        return ls.map(fn);
+    }
+    // iterables, incl. array
+    return new Map(fn, ls);
+});
 //-----------------------------------------------
 
 //-----------------------------------------------
 // FILTERING
 var Filter = _curry2(function Filter(f, xf) {
-  this.f = f;
-  this.xf = xf;
+    this.f = f;
+    this.xf = xf;
 });
 Filter.prototype.init = init;
 Filter.prototype.result = result;
 Filter.prototype.step = function(result, input) {
-  return this.f(input) ? this.xf.step(result, input) : result;
+    return this.f(input) ? this.xf.step(result, input) : result;
 };
-R.filter = _curry2(function(fn, ls) {});
-//-----------------------------------------------
-
-//-----------------------------------------------
-// FOLDING
-R.reduce = _curry3(function(fn, acc, ls) {});
-
+R.filter = _curry2(function(fn, ls) {
+    return new Filter(fn, ls);
+});
 //-----------------------------------------------
 
 //-----------------------------------------------
 // TAKING
-R.take = _curry2(function take(n, ls) {});
+var Take = _curry2(function Take(n, ls) {
+    this.n = n;
+    this.xf = ls;
+});
+Take.prototype.init = init;
+Take.prototype.result = result;
+Take.prototype.step = function(acc, x) {
+    return this.n-- > 0 ? this.xf.step(acc, x) :
+        x.__transducers_reduced__ ? x : {value: x, __transducers_reduced__: true};
+};
+R.take = _curry2(function take(n, ls) {
+    return new Take(n, ls);
+});
+//-----------------------------------------------
+
+//-----------------------------------------------
+// FOLDING
+R.reduce = _curry3(function(fn, acc, ls) {
+    if (Array.isArray(ls)) {
+        return arrayReduce(fn, acc, ls);
+    } else if (isIterable(ls)) {
+        return iterableReduce(fn, acc, ls);
+    } else {
+        throw new Error("didn't account for " + typeof ls);
+    }
+});
 //-----------------------------------------------
 
 //-----------------------------------------------
 // TRANSDUCING
-R.transduce = function(xf, fn, acc, ls) {
+R.transduce = _curry4(function(xf, fn, acc, ls) {
     return reduce(xf(fn), acc, ls);
-};
+});
 //-----------------------------------------------
 
 
