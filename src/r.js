bb6d5c6aede966b3fbdbd4a0db350be1d019deb8
buzzdecafe <m_hur@yahoo.com>
2014-12-19 19:31:34 -0500

    composition works

--- src/r.js
+++ src/r.js
@@ -206,7 +206,48 @@ R.transduce = _curry4(function(xf, fn, acc, ls) {
 });
 //-----------------------------------------------
 
+//-----------------------------------------------
+// COMPOSING
+function arity(n, fn) {
+    switch (n) {
+        case 0: return function() {return fn.apply(this, arguments);};
+        case 1: return function(a0) {void a0; return fn.apply(this, arguments);};
+        case 2: return function(a0, a1) {void a1; return fn.apply(this, arguments);};
+        case 3: return function(a0, a1, a2) {void a2; return fn.apply(this, arguments);};
+        case 4: return function(a0, a1, a2, a3) {void a3; return fn.apply(this, arguments);};
+        case 5: return function(a0, a1, a2, a3, a4) {void a4; return fn.apply(this, arguments);};
+        case 6: return function(a0, a1, a2, a3, a4, a5) {void a5; return fn.apply(this, arguments);};
+        case 7: return function(a0, a1, a2, a3, a4, a5, a6) {void a6; return fn.apply(this, arguments);};
+        case 8: return function(a0, a1, a2, a3, a4, a5, a6, a7) {void a7; return fn.apply(this, arguments);};
+        case 9: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8) {void a8; return fn.apply(this, arguments);};
+        case 10: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9) {void a9; return fn.apply(this, arguments);};
+        default: throw new Error('First argument to arity must be a non-negative integer no greater than ten');
+    }
+}
 
+function _createComposer(composeFunction) {
+    return function() {
+        switch (arguments.length) {
+            case 0: throw _noArgsException();
+            case 1: return arguments[0];
+            default:
+                var idx = arguments.length - 1, fn = arguments[idx], length = fn.length;
+                while (idx--) {
+                    fn = composeFunction(arguments[idx], fn);
+                }
+                return arity(length, fn);
+        }
+    };
+}
+
+function _compose(f, g) {
+    return function() {
+        return f.call(this, g.apply(this, arguments));
+    };
+}
+
+var compose = _createComposer(_compose);
+//-----------------------------------------------
 
 
 
