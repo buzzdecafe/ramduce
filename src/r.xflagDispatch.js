e876c10c13ef126b67d38870e4ae61f62422e9e3
Kevin Beaty <kevin@simplectic.com>
2014-12-26 16:18:34 -0600

    Delineate modules

--- src/r.xflagDispatch.js
+++ src/r.xflagDispatch.js
@@ -22,81 +22,97 @@ var R = {},
     pipe = rlib.pipe,
     slice = [].slice;
 
-var _appendXfArray = {
-    init: function(){
-      return [];
-    },
-    step: appendTo,
-    result: identity
-};
+//-----------------------------------------------
+// appendXf
+var _appendXf = R.appendXf = (function(){
+  var _appendXfArray = {
+      init: function(){
+        return [];
+      },
+      step: appendTo,
+      result: identity
+  };
 
-var _appendXfString = {
-    init: function(){
-      return '';
-    },
-    step: function(acc, x){
-      return acc + x;
-    },
-    result: identity
-};
+  var _appendXfString = {
+      init: function(){
+        return '';
+      },
+      step: function(acc, x){
+        return acc + x;
+      },
+      result: identity
+  };
 
-var _appendXfObject = {
-    init: function(){
-      return {};
-    },
-    step: mixin,
-    result: identity
-};
+  var _appendXfObject = {
+      init: function(){
+        return {};
+      },
+      step: mixin,
+      result: identity
+  };
 
-function _appendXf(obj){
-  // something like this...
-  // from github.com/transduce/transformer-protocol
-  // also see kevinbeaty/redispatch for potential late registration
-  if(isTransformer(obj)){
-    var xf = obj[symTransformer];
-    if(xf === void 0){
-      xf = obj;
+  function _appendXf(obj){
+    // something like this...
+    // from github.com/transduce/transformer-protocol
+    // also see kevinbeaty/redispatch for potential late registration
+    if(isTransformer(obj)){
+      var xf = obj[symTransformer];
+      if(xf === void 0){
+        xf = obj;
+      }
+      return xf;
     }
-    return xf;
+
+    if(Array.isArray(obj)){
+      return _appendXfArray;
+    }
+
+    if(is(String)){
+      return _appendXfString;
+    }
+
+    if(is(Object)){
+      return _appendXfObject;
+    }
+
+    throw new Error('Cannot create transformer for '+obj);
   }
 
-  if(Array.isArray(obj)){
-    return _appendXfArray;
+  return _appendXf;
+})();
+
+//-----------------------------------------------
+// _appendXfLastValue
+
+var _appendXfLastValue = (function(){
+  function LastValue(init){
+    this.initialValue = init;
+
+    // OR, allow overriding init as function if necessary
+    // Can use always(-1) etc. instead of -1 constant
+    //if(typeof init === 'function'){
+    //  this.init = init;
+    // }
+  }
+  LastValue.prototype.init = function(){ return this.initialValue; };
+  LastValue.prototype.step = function(acc, x){ return x; };
+  LastValue.prototype.result = identity;
+
+  function _appendXfLastValue(init){
+    // wrap in always because dispatch below calls function with
+    // obj to match _appendXf(obj)
+    return always(new LastValue(init));
   }
 
-  if(is(String)){
-    return _appendXfString;
-  }
+  return _appendXfLastValue;
+})();
 
-  if(is(Object)){
-    return _appendXfObject;
-  }
-
-  throw new Error('Cannot create transformer for '+obj);
-}
-R.appendXf = _appendXf;
-
-function LastValue(init){
-  this.initialValue = init;
-
-  // OR, allow overriding init as function if necessary
-  // Can use always(-1) etc. instead of -1 constant
-  //if(typeof init === 'function'){
-  //  this.init = init;
-  // }
-}
-LastValue.prototype.init = function(){ return this.initialValue; };
-LastValue.prototype.step = function(acc, x){ return x; };
-LastValue.prototype.result = identity;
-
-function _appendXfLastValue(init){
-  // wrap in always because dispatch below calls function with
-  // obj to match _appendXf(obj)
-  return always(new LastValue(init));
-}
-
+//-----------------------------------------------
+// _XF_FLAG_
 var _XF_FLAG_ = {};
 
+//-----------------------------------------------
+// _transduceDispatch
 function _transduceDispatch(xf, appendXf){
     return function(){
         var obj = this;
@@ -108,10 +124,14 @@ function _transduceDispatch(xf, appendXf){
     };
 }
 
+//-----------------------------------------------
+// _dispatchMarkConvert
 function _dispatchMarkConvert(fn) {
     return fn.__RAMDA_XF_FLAG_ === _XF_FLAG_ ? fn(_XF_FLAG_) : fn;
 }
 
+//-----------------------------------------------
+// _dispatchableN
 var _dispatchableN = (function(){
   function _dispatchableN(n, name, f) {
       var fn = _dispatchable(name, f);
@@ -173,79 +193,92 @@ var _dispatchableN = (function(){
 
   return _dispatchableN;
 })();
+
 //-----------------------------------------------
 // MAPPING
-function Map(f, xf) {
-    this.f = f;
-    this.xf = xf;
-}
-Map.prototype.init = init;
-Map.prototype.result = result;
-Map.prototype.step = function(result, input) {
-    return this.xf.step(result, this.f(input));
-};
 
-var xmap = _curry2(function(f, xf) {
-  return new Map(f, xf);
-});
-R.map = _dispatchableN(2, 'map', _transduceDispatch(xmap, _appendXf));
+var _xmap = (function(){
+  function Map(f, xf) {
+      this.f = f;
+      this.xf = xf;
+  }
+  Map.prototype.init = init;
+  Map.prototype.result = result;
+  Map.prototype.step = function(result, input) {
+      return this.xf.step(result, this.f(input));
+  };
+
+  return _curry2(function(f, xf) {
+    return new Map(f, xf);
+  });
+})();
+
+R.map = _dispatchableN(2, 'map', _transduceDispatch(_xmap, _appendXf));
 
 //-----------------------------------------------
 
 //-----------------------------------------------
 // FILTERING
-function Filter(f, xf) {
-    this.f = f;
-    this.xf = xf;
-}
-Filter.prototype.init = init;
-Filter.prototype.result = result;
-Filter.prototype.step = function(result, input) {
-    return this.f(input) ? this.xf.step(result, input) : result;
-};
-var xfilter = _curry2(function(f, xf) {
-  return new Filter(f, xf);
-});
-R.filter = _dispatchableN(2, 'filter', _transduceDispatch(xfilter, _appendXf));
+var _xfilter = (function(){
+  function Filter(f, xf) {
+      this.f = f;
+      this.xf = xf;
+  }
+  Filter.prototype.init = init;
+  Filter.prototype.result = result;
+  Filter.prototype.step = function(result, input) {
+      return this.f(input) ? this.xf.step(result, input) : result;
+  };
+  return _curry2(function(f, xf) {
+    return new Filter(f, xf);
+  });
+})();
+
+R.filter = _dispatchableN(2, 'filter', _transduceDispatch(_xfilter, _appendXf));
 //-----------------------------------------------
 
 //-----------------------------------------------
 // TAKING
-function Take(n, xf) {
-    this.n = n;
-    this.xf = xf;
-}
-Take.prototype.init = init;
-Take.prototype.result = result;
-Take.prototype.step = function(acc, x) {
-    return this.n-- > 0 ? this.xf.step(acc, x) :
-        x.__transducers_reduced__ ? x : {value: x, __transducers_reduced__: true};
-};
-var xtake = _curry2(function xtake(n, xf){
-  return new Take(n, xf);
-});
-R.take = _dispatchableN(2, 'take', _transduceDispatch(xtake, _appendXf));
+var _xtake = (function(){
+  function Take(n, xf) {
+      this.n = n;
+      this.xf = xf;
+  }
+  Take.prototype.init = init;
+  Take.prototype.result = result;
+  Take.prototype.step = function(acc, x) {
+      return this.n-- > 0 ? this.xf.step(acc, x) :
+          x.__transducers_reduced__ ? x : {value: x, __transducers_reduced__: true};
+  };
+  return _curry2(function xtake(n, xf){
+    return new Take(n, xf);
+  });
+})();
+
+R.take = _dispatchableN(2, 'take', _transduceDispatch(_xtake, _appendXf));
 
 //-----------------------------------------------
 // FINDING
-function Find(f, xf) {
-    this.f = f;
-    this.xf = xf;
-}
-Find.prototype.init = init;
-Find.prototype.result = result;
-Find.prototype.step = function(acc, x) {
-    if(this.f(x)){
-      return reduced(this.xf.step(acc, x));
-    }
-    return acc;
-};
-var xfind = _curry2(function xfind(f, xf){
-  return new Find(f, xf);
-});
-R.find = _dispatchableN(2, 'find', _transduceDispatch(xfind, _appendXfLastValue(void 0)));
+var _xfind = (function(){
+  function Find(f, xf) {
+      this.f = f;
+      this.xf = xf;
+  }
+  Find.prototype.init = init;
+  Find.prototype.result = result;
+  Find.prototype.step = function(acc, x) {
+      if(this.f(x)){
+        return reduced(this.xf.step(acc, x));
+      }
+      return acc;
+  };
+  return _curry2(function xfind(f, xf){
+    return new Find(f, xf);
 
-//-----------------------------------------------
+  });
+})();
+
+R.find = _dispatchableN(2, 'find', _transduceDispatch(_xfind, _appendXfLastValue(void 0)));
 
 //-----------------------------------------------
 // FOLDING
@@ -259,7 +292,6 @@ function _foldl(fn, acc, ls) {
         throw new Error("didn't account for " + typeof ls);
     }
 }
-//-----------------------------------------------
 
 //-----------------------------------------------
 // TRANSDUCING
@@ -268,18 +300,19 @@ R.transduce = _curry4(function(xf, fn, acc, ls) {
 });
 
 //-----------------------------------------------
-
-//-----------------------------------------------
+// tCompose
 R.tCompose = function xCompose() {
     var funcs = R.map(_dispatchMarkConvert, slice.call(arguments));
     return compose.apply(null, funcs);
 };
 
+//-----------------------------------------------
+// tPipe
 R.tPipe = function xPipe() {
     var funcs = R.map(_dispatchMarkConvert, slice.call(arguments));
     return pipe.apply(null, funcs);
 };
 
-R.compose = compose;
 //-----------------------------------------------
+R.compose = compose;
 module.exports = R;
